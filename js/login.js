7bef306f1a56c85e74f6695b0c36294d9f7a7bd2
brauliorrcp <69019980+brauliorrcp@users.noreply.github.com>
2020-08-10 20:41:49 -0300

    login nuevo

--- js/login.js
+++ js/login.js
@@ -4,3 +4,18 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
+
+function validateForm() {
+    var x = document.forms["login-form"]["usuario"].value;
+    if (x == "" || x == null) {
+      alert("Email must be filled out");
+      return false;
+    }
+
+    var y = document.forms["login-form"]["password"].value;
+    if (y == "" || y == null) {
+      alert("Password must be filled out");
+      return false;
+    }
+
+  }
