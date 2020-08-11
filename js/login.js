5c945e41b64ef778abfd343acac7b0adcc367113
brauliorrcp <69019980+brauliorrcp@users.noreply.github.com>
2020-08-11 10:05:39 -0300

    validacion login

--- js/login.js
+++ js/login.js
@@ -5,17 +5,32 @@ document.addEventListener("DOMContentLoaded", function(e){
 
 });
 
-function validateForm() {
-    var x = document.forms["login-form"]["usuario"].value;
+function validate(value, name) {
+    if (value == "" || value == null) {
+      document.getElementById("error").innerHTML = "Debe ingresar su " + name;
+      return false;
+    }
+  }
+
+  function validateForm() {
+    var x = document.forms["login-form"]["nombre"].value;
     if (x == "" || x == null) {
-      alert("Debe ingresar el usuario");
+      document.getElementById("error").innerHTML = "Debe ingresar su usuario";
       return false;
     }
 
-    var y = document.forms["login-form"]["password"].value;
-    if (y == "" || y == null) {
-      alert("Debe ingresar la contraseña");
+    var x = document.forms["login-form"]["password"].value;
+    if (x == "" || x == null) {
+      document.getElementById("error").innerHTML = "Debe ingresar su contraseña";
       return false;
     }
 
+  }
+
+  function inicioSesion() {
+    if ( !validateForm() ) {
+      document.addEventListener('DOMContentLoaded', () => {
+        window.location = 'index.html'
+    })
+    }
   }
