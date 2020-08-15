3907e3e47c8c86aa1f8273731b43d865ae2d5445
brauliorrcp <69019980+brauliorrcp@users.noreply.github.com>
2020-08-15 12:39:00 -0300

    login fix

--- js/login.js
+++ js/login.js
@@ -13,22 +13,16 @@ function validate(value, name) {
   }
 
   function validateForm() {
-    var x = document.forms["login-form"]["nombre"].value;
+    var x = document.forms["login-form"]["usuario"].value;
     if (x == "" || x == null) {
       document.getElementById("error").innerHTML = "Debe ingresar su usuario";
       return false;
     }
 
-    var x = document.forms["login-form"]["password"].value;
-    if (x == "" || x == null) {
+    var y = document.forms["login-form"]["password"].value;
+    if (y == "" || y == null) {
       document.getElementById("error").innerHTML = "Debe ingresar su contraseña";
       return false;
     }
 
-  }
-
-  function inicioSesion() {
-    if ( !validateForm() ) {
-      loginredirect = true;
-    }
   }
