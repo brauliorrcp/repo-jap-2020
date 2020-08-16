1f14f91f427d1ebaf9b324b8f1a5331563773f75
brauliorrcp <69019980+brauliorrcp@users.noreply.github.com>
2020-08-16 13:41:21 -0300

    correcc json 2

--- js/products.js
+++ js/products.js
@@ -17,10 +17,10 @@ function showCategoriesList(array){
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
                         <h4 class="mb-1">`+ category.name +`</h4>
-                        <small class="text-muted">` + category.soldCount + ` artículos vendidos</small>
+                        <small class="text-muted">` + category.currency + " " + category.cost + ` </small>
                     </div>
                     <div> ` + category.description + ` </div>
-                    <small class="text-muted">` + category.currency + " " + category.cost + ` </small>
+                    <small class="text-muted">` + category.soldCount + ` artículos vendidos</small>
                 </div>
             </div>
         </div>
