176626fd475e5bb64b5ba5121f68cabac6cfbfef
brauliorrcp <69019980+brauliorrcp@users.noreply.github.com>
2020-11-08 22:42:50 -0300

    entrega 7

--- js/my-profile.js
+++ js/my-profile.js
@@ -2,46 +2,34 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    datosUsuario = JSON.parse(localStorage.getItem("JSONDatos"));
+
+    document.getElementById("mostrarnombre").innerHTML = datosUsuario.name;
+    document.getElementById("mostraredad").innerHTML = datosUsuario.age;
+    document.getElementById("mostrarmail").innerHTML = datosUsuario.mail;
+    document.getElementById("mostrartel").innerHTML = datosUsuario.cel;
 
 });
 
-miStorage = window.localStorage;
+document.getElementById("nombredeperfil").innerHTML = miStorage.getItem("usuario");
 
-var datosUsuario = {
-    "name": "",
-    "age": null,
-    "mail": "",
-    "cel": "",
-}
+let milocStorage = window.localStorage;
 
-function datosPerfil() {
 
-    let nombre = document.getElementById("nomyap").value ;
-    let edad = document.getElementById("edad").value;
-    let email = document.getElementById("email").value;
-    let tel = document.getElementById("tel").value;
+function datosPerfil2() {
 
-    localStorage.setItem("nomyap", nombre);
-    localStorage.setItem("edad", edad);
-    localStorage.setItem("email", email);
-    localStorage.setItem("tel", tel);
+    var datosUsuario = {
+        "name": document.getElementById("nomyap").value,
+        "age": document.getElementById("edad").value,
+        "mail": document.getElementById("email").value,
+        "cel": document.getElementById("tel").value,
+    }
 
-    datosUsuario.name = localStorage.getItem("nomyap");
-    datosUsuario.age = localStorage.getItem("edad");
-    datosUsuario.mail = localStorage.getItem("email");
-    datosUsuario.cel = localStorage.getItem("tel");
+    if(validarUsuario()) {
+    localStorage.setItem("JSONDatos", JSON.stringify(datosUsuario));
 
-document.getElementById("mostrarnombre").innerHTML = localStorage.getItem("nomyap");
-document.getElementById("mostraredad").innerHTML = localStorage.getItem("edad");
-document.getElementById("mostrarmail").innerHTML = localStorage.getItem("email");
-document.getElementById("mostrartel").innerHTML = localStorage.getItem("tel");
-
-}
-
-document.getElementById("mostrarnombre").innerHTML = localStorage.getItem("nomyap");
-document.getElementById("mostraredad").innerHTML = localStorage.getItem("edad");
-document.getElementById("mostrarmail").innerHTML = localStorage.getItem("email");
-document.getElementById("mostrartel").innerHTML = localStorage.getItem("tel");
+    location.href = "my-profile.html";
+}}
 
 
 function borrarDatos() {
@@ -53,6 +41,51 @@ function borrarDatos() {
     document.getElementById("mostrarmail").innerHTML = "";
     document.getElementById("mostrartel").innerHTML = "";
 
+}
+
+
+
+function validarUsuario() {
+    var x = document.getElementById("nomyap").value;
+    if (x == "") {
+        document.getElementById("errorperfil").innerHTML = "Debe ingresar su nombre y apellido";
+        document.getElementById("nomyap").style.borderColor = "red";
+      return false;
+    } else {
+        document.getElementById("errorperfil").innerHTML = "";
+      document.getElementById("nomyap").style.borderColor = "green";
+    }
+
+    var y = document.getElementById("edad").value;
+    if (y == "") {
+      document.getElementById("errorperfil").innerHTML = "Debe ingresar su edad";
+      document.getElementById("edad").style.borderColor = "red";
+      return false;
+    } else {
+        document.getElementById("errorperfil").innerHTML = "";
+        document.getElementById("edad").style.borderColor = "green";
+    }
+
+    var y = document.getElementById("email").value;
+    if (y == "") {
+      document.getElementById("errorperfil").innerHTML = "Debe ingresar su email";
+      document.getElementById("email").style.borderColor = "red";
+      return false;
+    } else {
+        document.getElementById("errorperfil").innerHTML = "";
+        document.getElementById("email").style.borderColor = "green";
+    }
+
+    var y = document.getElementById("tel").value;
+    if (y == "") {
+      document.getElementById("errorperfil").innerHTML = "Debe ingresar su teléfono";
+      document.getElementById("tel").style.borderColor = "red";
+      return false;
+    } else {
+        document.getElementById("errorperfil").innerHTML = "";
+        document.getElementById("tel").style.borderColor = "green";
+        return true;
+    }
 }
 
 
